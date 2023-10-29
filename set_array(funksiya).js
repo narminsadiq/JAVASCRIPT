738b2a099913fdc9aa32e35275a16b538514297c
narminsadig <149061993+narminsadiq@users.noreply.github.com>
2023-10-29 17:57:54 +0400

    tekrarlanmayan ARRAY(SET ile)

---
+++ set_array(funksiya).js
@@ -0,0 +1,13 @@
+function unique(arr) {
+     return Array.from(new Set(arr));
+   }
+   let siyahi=[
+     "nermin",
+     "roya",
+     "aslan",
+     "nermin",
+     "leyla",
+     "lale",
+     "aslan"
+]
+console.log(unique(siyahi));
